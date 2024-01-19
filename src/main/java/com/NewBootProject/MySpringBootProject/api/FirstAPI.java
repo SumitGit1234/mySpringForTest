f17a279c9df7a88355f69556fd0c5ab587d563b9
SumitGit1234 <marik.supriya12@gmail.com>
2024-01-19 17:15:30 +0530

    2nd commit

--- src/main/java/com/NewBootProject/MySpringBootProject/api/FirstAPI.java
+++ src/main/java/com/NewBootProject/MySpringBootProject/api/FirstAPI.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class FirstAPI {
 	
-	@RequestMapping("/testing")
+	@RequestMapping("/testing-one")
 	public String testJenkins(){
 		return "Hey there! I am Sumit:))";
 	}
